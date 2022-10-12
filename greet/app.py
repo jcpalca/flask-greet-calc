6d356044d20abc0a1ed5258e25eb03d2fda91e29
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-10-12 12:00:24 -0700

    Make welcome back route

--- greet/app.py
+++ greet/app.py
@@ -14,3 +14,9 @@ def welcome_home():
     """Return simple 'welcome home' greeting"""
     html = "<html><body><h1>welcome home</h1></body></html>"
     return html
+
+@app.get("/welcome/back")
+def welcome_back():
+    """Return simple 'welcome back' greeting"""
+    html = "<html><body><h1>welcome back</h1></body></html>"
+    return html
